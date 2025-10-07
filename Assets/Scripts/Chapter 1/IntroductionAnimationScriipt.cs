24ec429026fbbc1522dcf605cc55e34522fec185
eri-61 <alien.erikatuscano@gmail.com>
2025-10-07 17:23:07 +0800

    [update] buttons
    - added use item button to inventory prefab
    - changed load checkpoint to load chapter in main menu
    - added and coded skip button to intro animation

--- Assets/Scripts/Chapter 1/IntroductionAnimationScriipt.cs
+++ Assets/Scripts/Chapter 1/IntroductionAnimationScriipt.cs
@@ -1,19 +1,35 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
-
+using UnityEngine.UI;
 public class IntroductionAnimationScriipt : MonoBehaviour
 {
     public float waitTime = 15.5f;
+    public float skipButton_waitTime = 2f;
+
+    public Button skipButton;
 
     void Start()
     {
         StartCoroutine(Wait());
+        StartCoroutine(WaitForSkip());
+        skipButton.onClick.AddListener(SkipIntro);
+    }
+
+    IEnumerator WaitForSkip()
+    {
+        yield return new WaitForSeconds(skipButton_waitTime);
+        skipButton.gameObject.SetActive(true);
     }
 
     IEnumerator Wait()
     {
         yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene(4);
+    }
+
+    void SkipIntro()
+    {
         SceneManager.LoadScene(4);
     }
 }
