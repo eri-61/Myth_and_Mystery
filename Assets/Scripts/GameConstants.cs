186d3c130c502f91a5a09f9019cc17bf42f3c819
Erwin Mendoza <khronosangel@gmail.com>
2025-10-26 07:48:18 +0800

    [Update] Game Flow Fixes and more

--- Assets/Scripts/GameConstants.cs
+++ Assets/Scripts/GameConstants.cs
@@ -11,10 +11,14 @@ public static class GameConstants
     /// </summary>
     public static string[,] ChapterDialogs { get {
             string[,] chapDia = new string[4, 10];
-            chapDia[0, 0] = "VN_Office";
-            chapDia[0, 1] = "VN_OfficeD2";
-            chapDia[0, 2] = "IS_Camp";
+            chapDia[0, 0] = "";
+            chapDia[0, 1] = "VN_Office";
+            chapDia[0, 2] = "VN_OfficeD2";
             chapDia[0, 3] = "IS_Camp";
+            chapDia[0, 4] = "VN_Camp";
+            chapDia[0, 5] = "VN_Lambago";
+            chapDia[0, 6] = "VN_Clearing";
+            chapDia[0, 7] = "Battle_Scene";
 
             return chapDia;
         }     
